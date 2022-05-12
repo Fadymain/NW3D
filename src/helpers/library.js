fbbe31a43d0e23850d3837490ab48021cb782f4a
Adetola <adetolafadairo@gmail.com>
2022-05-11 20:48:17 -0700

    creating library sample

---
+++ src/helpers/library.js
@@ -0,0 +1,18 @@
+import titanicS from "../images/titanicScene.png";
+import tita from "../images/tit.png";
+import movieLogo from "../images/movieLogo.png";
+
+export const movies = [
+  {
+    Movie: "",
+    Thumbnail: tita,
+    Scene: titanicS,
+    Logo: movieLogo,
+    Name: "Tutanic",
+    Description: "Description Description Description Description Description Description Description Description",
+    Genre: "Romance, Drama",
+    Actors: "Kate Winslet, Leonardo DiCapro",
+    Yeear: "1998",
+    Duration: "3h 14min",
+  },
+];

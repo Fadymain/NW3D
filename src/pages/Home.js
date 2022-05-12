f24e521e7af6252306e5878f039ba14b7f582c06
Adetola <adetolafadairo@gmail.com>
2022-05-11 20:30:53 -0700

    Adding Tab and TabList to file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import "./Home.css";
 import { Logo } from '../images/Netflix';
-import { ConnectButton, Icon } from "web3uikit"
+import { ConnectButton, Icon, Tab, TabList } from "web3uikit"
 
 const Home = () => {
 
 return(
   <>
-  <div className='logo'>
+  <div className="logo">
     <Logo />
   </div>
-  <div className='connect'>
-    <Icon fill='#ffffff' size={24} svg='bell' />
+  <div className="connect">
+    <Icon fill="#ffffff" size={24} svg="bell" />
     <ConnectButton />
   </div>
-  <div className="container">
-     <Link to="/player" className="link">Player Page</Link>
-     Hello Netflix
+  <div className="topBanner">
+     <TabList defaultActiveKey={1} tabStyle="bar">
+       <Tab tabKey={1} tabName={"Moviees"}></Tab>
+       <Tab tabKey={2} tabName={"Series"} isDisabled={true}></Tab>
+       <Tab tabKey={3} tabName={"MyList"}></Tab>
+     </TabList>
   </div>
   </>
 )

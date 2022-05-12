28965cc9f2a8de448bed781b32b9df550f8b856c
Adetola <adetolafadairo@gmail.com>
2022-05-11 20:13:18 -0700

    importing and adding ConnectionButton and Icon from web3uikit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import "./Home.css";
 import { Logo } from '../images/Netflix';
+import { ConnectButton, Icon } from "web3uikit"
 
 const Home = () => {
 
@@ -9,6 +10,10 @@ return(
   <>
   <div className='logo'>
     <Logo />
+  </div>
+  <div className='connect'>
+    <Icon fill='#ffffff' size={24} svg='bell' />
+    <ConnectButton />
   </div>
   <div className="container">
      <Link to="/player" className="link">Player Page</Link>

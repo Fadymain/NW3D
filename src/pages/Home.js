d2249e52511c5e0c1ab4f74193c8c04d856e79ed
Adetola <adetolafadairo@gmail.com>
2022-05-12 22:55:24 -0700

    adding movie info to the Modal when displayed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,79 +12,114 @@ const Home = () => {
   const [selectedFilm, setSelectedFilm] = useState();
 
 
-return(
-  <>
-  <div className="logo">
-    <Logo />
-  </div>
-  <div className="connect">
-    <Icon fill="#ffffff" size={24} svg="bell" />
-    <ConnectButton />
-  </div>
-  <div className="topBanner">
-     <TabList defaultActiveKey={1} tabStyle="bar">
-       <Tab tabKey={1} tabName={"Movies"}>
-         <div className="scene">
-           <img src={movies[0].Scene} className="sceneImg" alt=''/>
-           <img src={movies[0].Logo} className="sceneLogo" alt=''/>
-           <p className="sceneDesc">{movies[0].Description}</p>
-           <div className="playButton">
-            <Button
-              icon="chevronRightX2"
-              text="Play"
-              theme="secondary"
-              type="button"
-            />
-            <Button
-              icon="plus"
-              text="Add to My List"
-              theme="translucent"
-              type="button"
-            />
-           </div>
-         </div>
+  return (
+    <>
+      <div className="logo">
+        <Logo />
+      </div>
+      <div className="connect">
+        <Icon fill="#ffffff" size={24} svg="bell" />
+        <ConnectButton />
+      </div>
+      <div className="topBanner">
+        <TabList defaultActiveKey={1} tabStyle="bar">
+          <Tab tabKey={1} tabName={"Movies"}>
+            <div className="scene">
+              <img src={movies[0].Scene} className="sceneImg" alt='' />
+              <img src={movies[0].Logo} className="sceneLogo" alt='' />
+              <p className="sceneDesc">{movies[0].Description}</p>
+              <div className="playButton">
+                <Button
+                  icon="chevronRightX2"
+                  text="Play"
+                  theme="secondary"
+                  type="button"
+                />
+                <Button
+                  icon="plus"
+                  text="Add to My List"
+                  theme="translucent"
+                  type="button"
+                />
+              </div>
+            </div>
 
-        <div className="title">
-          Movies
-        </div>
-        <div className="thumbs">
-         {movies &&
-         movies.map((e) => {
-           return(
-             <img
-              src={e.Thumnbnail}
-              className="thumbnail"
-              alt=''
-              onClick={() => {
-                setSelectedFilm(e);
-                setVisible(true);
-              }}
-              />
-           )
-         })
-         }
-        </div>
+            <div className="title">
+              Movies
+            </div>
+            <div className="thumbs">
+              {movies &&
+                movies.map((e) => {
+                  return (
+                    <img
+                      src={e.Thumnbnail}
+                      className="thumbnail"
+                      alt=''
+                      onClick={() => {
+                        setSelectedFilm(e);
+                        setVisible(true);
+                      }}
+                    />
+                  )
+                })
+              }
+            </div>
 
-       </Tab>
-       <Tab tabKey={2} tabName={"Series"} isDisabled={true}></Tab>
-       <Tab tabKey={3} tabName={"MyList"}></Tab>
-     </TabList>
-     {selectedFilm && (
-       <div className="modal">
-        <Modal
-          onCloseButtonPressed={() => setVisible(false)}
-          isVisible={visible}
-          hasFooter={false}
-          width="1000px"
-        >
-      
+          </Tab>
+          <Tab tabKey={2} tabName={"Series"} isDisabled={true}></Tab>
+          <Tab tabKey={3} tabName={"MyList"}></Tab>
+        </TabList>
+        {selectedFilm && (
+          <div className="modal">
+            <Modal
+              onCloseButtonPressed={() => setVisible(false)}
+              isVisible={visible}
+              hasFooter={false}
+              width="1000px"
+            >
 
-        </Modal>
-       </div>
-     )}
-  </div>
-  </>
-)
+              <div className="modalContent">
+                <img src={selectedFilm.Scene} className="modalImg" alt='' />
+                <img src={selectedFilm.Logo} className="modalLogo" alt='' />
+                <div className="modalPlayButton">
+                  <Button
+                    icon="chevronRightX2"
+                    text="Play"
+                    theme="secondary"
+                    type="button"
+                  />
+                  <Button
+                    icon="plus"
+                    text="Add to My List"
+                    theme="translucent"
+                    type="button"
+                  />
+                </div>
+
+                <div className="movieInfo">
+                  <div className="description">
+                    <div className="details">
+                      <span>{selectedFilm.Year}</span>
+                      <span>{selectedFilm.Duration}</span>
+                    </div>
+                    {selectedFilm.Description}
+                  </div>
+                  <div className="detailedInfo">
+                      Genre:
+                      <span className="deets">{selectedFilm.Genre}</span>
+                      <br/>
+                      Actors:
+                      <span className="deets">{selectedFilm.Actors}</span>
+                  </div>
+                </div>
+              </div>
+
+            </Modal>
+          </div>
+        )}
+      </div>
+    </>
+  )
 }
 
 export default Home;
